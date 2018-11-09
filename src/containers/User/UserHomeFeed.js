667bcf5fa89971e3ec2e1f65564addf06dd35efc
ARCHANA AHLAWAT <archana.ahlawat7@gmail.com>
2018-11-08 23:46:09 -0500

    testing backend endpoints

--- src/containers/User/UserHomeFeed.js
+++ src/containers/User/UserHomeFeed.js
@@ -3,6 +3,10 @@ import { List, Avatar, Icon } from "antd";
 import PostInteraction from "../../components/PostInteraction";
 
 const listData = [];
+//const API = 'localhost/posts/search/getUserFeed';
+const API = 'http://localhost:8080/posts/search/getUserFeed?username=john.doe';
+//const data = {username: 'john.doe'};
+
 for (let i = 0; i < 10; i++) {
   listData.push({
     href: "http://ant.design",
@@ -23,8 +27,27 @@ class UserHomeFeed extends Component {
   }
 
   componentDidMount() {
-    //Get information about posts!
+      fetch(API, {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+          },
+          mode: 'no-cors',
+          body: JSON.stringify({
+              'client_id': 'oasys',
+              'client_secret': 'XY7kmzoNzl100@localhost:8080/oauth/token',
+              'grant_type': 'password'
+          })
+      })
+
+          .then(function(response) {
+            return response;
+        })
+        .then(function(myJson) {
+            console.log(JSON.stringify(myJson));
+        });
   }
+
   render() {
     return (
       <List
