11a82120832f2f2da8aa5a9ee47ebbe2cee99a11
Ben Schwennesen <bas65@duke.edu>
2018-12-10 02:46:25 -0500

    Add a page for posts

---
+++ src/components/PostComponent.js
@@ -0,0 +1,42 @@
+import {
+  List, Icon, Avatar
+} from 'antd';
+import React from "react";
+
+const IconText = ({type, text}) => (
+  <span>
+    <Icon type={type} style={{marginRight: 8}}/>
+    {text}
+  </span>
+);
+export default ({post}) => {
+  if (!post.creator) {
+    return null;
+  } else {
+    return (
+      <List
+        itemLayout="vertical"
+        size="large"
+        dataSource={[post]}
+        renderItem={item => (
+          <List.Item
+            key={item.pid}
+            actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />,
+              <IconText type="message" text="2" />]}
+            extra={<img width={272} src={item.flock.photoPath}/>}
+          >
+            <List.Item.Meta
+              // TODO: why doesn't the avatar show up? If List is horizontal instead of vertical it works :'(
+              avatar={<Avatar src={item.creator.photoPath}/>}
+              title={<a href={"/flock/" + item.flock.name}>{item.flock.name}</a>}
+              description={item.creator.name}
+            />
+            {item.body}
+          </List.Item>
+        )}
+
+      />
+
+    )
+  }
+}
